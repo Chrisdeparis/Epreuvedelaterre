ae527be9c97ff73c04b8b749feeaf260662df536
maescristro <milfortchristian@gmail.com>
2022-10-17 07:49:57 +0200

    terre08.js

--- terre08.js
+++ terre08.js
@@ -2,11 +2,22 @@
     
 let args =  process.argv.slice(2);
 
-
 function func1(){
 
+    let numberObject = new Number(args);
+    
+    let value = numberObject.valueOf();
+
+    let items = args.length;
+
     let number = args.toString().length;
+
+    if(number == 0 || items > 1 || !isNaN(value)){
+        console.log ('erreur.');
+        return;
+    }
     console.log(number);
+
 }
 
 func1(args);
