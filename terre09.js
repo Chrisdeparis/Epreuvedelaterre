cc95c94fb07056e03c481236cd22b1ecfbdf260d
maescristro <milfortchristian@gmail.com>
2022-10-17 08:15:11 +0200

    terre09.js

---
+++ terre09.js
@@ -0,0 +1,20 @@
+// Créez un programme qui affiche le résultat d’une puissance.
+//L’exposant ne pourra pas être négatif.
+
+let args =  process.argv.slice(2);
+
+function func1(){
+    // console.log(args);
+    let number = args[0];
+    let exposant = args[1];
+    if(exposant > 0){
+        let resultat = number ** exposant;
+        console.log(resultat);
+    } else {
+        console.log('L’exposant ne pourra pas être négatif.');
+    }
+    
+
+}
+
+func1(args);
